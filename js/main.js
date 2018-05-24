dc9e9a3de8ff44cd16c2ad526b056b99111ebf5d
Joe DeSoto <modulusman@gmail.com>
2018-05-24 11:25:14 -0700

    adds bootstrap, removes demo code for YT video

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// function demo() {
+//   var rand_int = Math.floor(Math.random()*5)+1;
+//   var e = document.getElementById("res_selector");
+//   var selected_res = e.options[e.selectedIndex].text;
+//   document.getElementById('result_text').innerHTML = selected_res + ' is ' + rand_int + ' feet down';
+// }
