4df6dda1c9efff6252178d4f5bbe119d14832344
Joe DeSoto <modulusman@gmail.com>
2018-06-30 10:29:01 -0700

    removed old bg images, added actual ones from CP

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// initialize ftDown here, since it's used by both functions
-var ftDown;
+// initialize ftDown here, since it's used by both functions. also initialize empty output array
+var ftDown, output = [];
 // south reservoirs object
-// access 1264 key by running: south[1264]. this returns the information for 1264, as pulled from the smartcover site. the format of the returned info is {1264: 13.3}, where 1264 is a string and 13.3 is a number. to get jsut 13.3, run south[1264]['1264']
+// access 1264 key by running: south[1264]. this returns the information for 1264, as pulled from the smartcover site
 var south = {
   1264: 13.3,
   13151: 21.5,
@@ -25,8 +25,6 @@ var north = {
   62310: 23.5,
   63210: 19.8,
 } // closes north object
-// initialize empty array for storing reservoir keys from north or south object
-var output = [];
 // jquery functions
 $(document).ready(function() {
   // hide the error message for empty input
@@ -38,6 +36,7 @@ $(document).ready(function() {
     // add class of change to menu-container div when it's clicked. This will trigger the css change selector
     $('.menu-container').toggleClass('change');
   }) // ends click function for hamburger menu
+
   // when the user clicks south button in the nav
   $('li#south_button').click(function() {
     $('li#south_button').addClass('active').css('font-weight','bold');
@@ -54,6 +53,7 @@ $(document).ready(function() {
     // join returns a string from an array, in this case the output array. so, at this point, the output array now holds all the keys from the south object. join('') places all the strings next to each other without whitespace. this allows each one to be used as an option on the res_dropdown select. use html since the markup is being modified. another advantage of using this method that i've read, is the DOM gets modified only once. changing and modifying the dom should be done as little as possible. other answers on SO showed an append to the DOM each time a new option was to be inserted. that is far more "expensive" than collecting all items to be used as options and updating the DOM once.
     $('#res_dropdown').html(output.join(''));
   }) // ends south button click function
+
   // when the user clicks north button in the nav
   $('li#north_button').click(function() {
     $('li#south_button').removeClass('active').css('font-weight','normal');
@@ -67,7 +67,8 @@ $(document).ready(function() {
     }); // ends south function for  each key, value
     // join returns a string from an array, in this case the output array. so, at this point, the output array now holds all the keys from the north object. join('') places all the strings next to each other without whitespace. this allows each one to be used as an option on the res_dropdown select. use html since the markup is being modified
     $('#res_dropdown').html(output.join(''));
-  }) // ends north button click function
+  }); // ends north button click function
+
    // when the feet down button is clicked
   $("button").click(function() {
     // sanity check for empty input field for a reservoir. this shows the bootstrap alert and also closes it after 2 seconds, 2000 milliseconds. this works once, but doesn't keep working if the form is still left blank and the feet down button is repeatedly clicked.
@@ -78,7 +79,7 @@ $(document).ready(function() {
       setTimeout(function() {
         $('#alert_message').hide();
       }, 3000);
-    }
+    } // ends if
   	// run the resLevel function when the button is clicked. grab the text of the selected option from the dropdown and use this as the first parameter of the resLevel function. the second parameter is the inputted smartcover level from the user. grab that value using .val()
   	resLevel($('#res_dropdown option:selected').text(), $('#sc_level').val());
   }); // ends button click function
@@ -86,16 +87,16 @@ $(document).ready(function() {
 
 // set the measurement results to be outputted to the dom. note that ftDown is global and will be passed in once the resLevel function is called by the button click
 function resultText(level) {
-	// sanity check for the singular unit
-    if(ftDown === 1) {
-    	// send the result of the calculation to the dom using html, and use html() in order to render the added span tag
-      	$('#result').html($('#res_dropdown option:selected').text() + ' is ' + "<span class='num_text'>" + ftDown + "</span>" + ' foot down');
-    } else {
-     	// use toFixed for rounding the displayed results
-      	ftDown = ftDown.toFixed(2);
-        // send the result of the calculation to the dom using html, and use html() in order to render the added span tag
-      	$('#result').html($('#res_dropdown option:selected').text() + ' is ' + "<span class='num_text'>" + ftDown + "</span>" + ' feet down');
-    } // ends else
+  // sanity check for the singular unit
+  if(ftDown === 1) {
+	  // send the result of the calculation to the dom using html, and use html() in order to render the added span tag
+  	$('#result').html($('#res_dropdown option:selected').text() + ' is ' + "<span class='num_text'>" + ftDown + "</span>" + ' foot down');
+  } else {
+ 	  // use toFixed for rounding the displayed results
+  	ftDown = ftDown.toFixed(2);
+    // send the result of the calculation to the dom using html, and use html() in order to render the added span tag
+  	$('#result').html($('#res_dropdown option:selected').text() + ' is ' + "<span class='num_text'>" + ftDown + "</span>" + ' feet down');
+  } // ends else
 } // ends resultText
 
 // function for reservoir levels. res is a user selected reservoir, reading is the most up to date level on the graph for that reservoir from the SmartCover site
